dc9e24a133a9db31b99145d4fd938784c51d2435
abraham Perez Quiroz <Abrahamperezquiroz0@gmail.com>
2026-03-12 19:34:54 -0600

    Conectado a firebase

---
+++ Lib/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDU07ZRTprWTEqUUrOX43-kXrqRNHOk6nc",
+  authDomain: "speedy-go.firebaseapp.com",
+  projectId: "speedy-go",
+  storageBucket: "speedy-go.firebasestorage.app",
+  messagingSenderId: "151732953330",
+  appId: "1:151732953330:web:6d8f189040b5e9024bb06b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
